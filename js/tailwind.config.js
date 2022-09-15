9049222a57abdda9ea683b0e7ae1888629799f14
monju819 <sajidhasan819@gmail.com>
2022-09-15 14:24:53 +0600

    responsive prb

---
+++ js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        lightblue: "#5C6A92",
+        primary: "#F40404",
+        yallow: "#FFB800",
+      },
+      maxWidth: {
+        container: "1140px",
+      },
+    },
+  },
+};
